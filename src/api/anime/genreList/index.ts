0289cf5581dfa413b036575720f079c6a00e46dd
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-01-30 14:02:41 -0300

    Genre List

--- src/api/anime/genreList/index.ts
+++ src/api/anime/genreList/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, Router } from "express";
+import { getGenreList } from "animu-desu";
 
 const router: Router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-    res.json("this is the popular route");
-  });
-  
-  
+router.get("/", async (req: Request, res: Response) => {
+  await getGenreList()
+    .then((genreList) => res.json(genreList))
+    .catch((err) => res.json(err));
+});
+
 export { router };
