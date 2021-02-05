237fb0b0b3472e09c33063068cb439c5293e807e
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-02-04 21:35:52 -0300

    Better way to get the exact id
    Added pseudo algorithm to get the id asclose as possible while being fast

--- src/api/anime/getId/index.ts
+++ src/api/anime/getId/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from "express";
+import express, { json, Request, Response, Router } from "express";
 import { getAnimeDetails, search } from "animu-desu";
 import { AnimeAndDate } from "animu-desu/lib/types";
 
@@ -8,43 +8,84 @@ router.get(
   "/:title/:totalEpisodes/:otherNames/:year",
   async (req: Request, res: Response) => {
     const { title, totalEpisodes, otherNames, year } = req.params;
+    let anilist = {} as animeFromAnilist;
+
     try {
-      parseInt(year);
-      parseInt(totalEpisodes);
+      anilist = {
+        otherNames: decodeURIComponent(otherNames),
+        title: decodeURIComponent(title),
+        year: parseInt(year),
+        totalEpisodes: parseInt(totalEpisodes),
+      };
     } catch (err) {
       res.json(err);
     }
     let results = [] as AnimeAndDate[];
-    let idsFound = [] as string[];
+    let found = [] as AnimeAndDate[];
+    let toReturn = [] as string[];
 
     await search(decodeURIComponent(title), 1)
       .then((data) => (results = data))
       .catch((err) => res.json(err));
 
-    await Promise.all(
-      results.map(async (animeFromQuery) => {
-        if (animeFromQuery.released === parseInt(year)) {
-          await getAnimeDetails(animeFromQuery.id)
-            .then((details) => {
-              if (
-                details.totalEpisodes === parseInt(totalEpisodes) ||
-                details.totalEpisodes === parseInt(totalEpisodes) - 1
-              ) {
-                decodeURIComponent(otherNames).split(",").map((name) => {
-                  if (details.otherNames.includes(name)) {
-                    console.log("returnin " + details.id);
-                    idsFound.push(details.id);
-                  }
-                });
-              }
-            })
-            .catch((err) => res.json(err));
-        }
+    await compareYearAndTotalEpisodes(anilist, results).then((matching) =>
+      matching.map((match) => {
+        found.push(match)
+        console.log(JSON.stringify(match))
       })
     );
-      
-    res.json(idsFound);
+
+    if (found.length > 2) {
+      await Promise.all(
+        found.map(async (animeFromQuery) => {
+          await getAnimeDetails(animeFromQuery.id)
+            .then((details) => {
+              anilist.otherNames
+                .split(",")
+                .map((name) => {
+                  if (details.otherNames.includes(name)) {
+                    toReturn.push(details.id);
+                  }
+                });
+            })
+            .catch((err) => res.json(err));
+        })
+      );
+    } else {
+      found.map((match) => {
+        toReturn.push(match.id);
+      });
+    }
+
+    res.json(toReturn);
   }
 );
+
+interface animeFromAnilist {
+  title: string;
+  totalEpisodes: number;
+  otherNames: string;
+  year: number;
+}
+
+async function compareYearAndTotalEpisodes(
+  anilist: animeFromAnilist,
+  gogoResults: AnimeAndDate[]
+): Promise<AnimeAndDate[]> {
+  let matching = [] as AnimeAndDate[];
+  await Promise.all(gogoResults.map(async (result) => {
+    if (result.released === anilist.year) {
+      await getAnimeDetails(result.id).then((details) => {
+        if (
+          details.totalEpisodes === anilist.totalEpisodes ||
+          details.totalEpisodes === anilist.totalEpisodes - 1
+        )
+          matching.push(result);
+      });
+    }
+  }));
+  console.log("im returnin xd")
+  return matching;
+}
 
 export { router };
