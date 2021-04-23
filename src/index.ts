6dd3dfdc315e658f567f5e22154970b95f9dd030
unknown <mateocarriqui7@gmail.com>
2021-04-23 00:04:47 -0300

    export express app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-require("dotenv").config();
-import express, {Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { router } from "./api";
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const app: Application = express();
 
 app.use(cors());
 
-app.use('/api',router);
+app.use("/api", router);
 
-app.listen(port, () => console.log(`express is listening in port: ${port}`));
+export { app };
+//usage : import { app } from "./index.ts"
+// app.listen(port);
